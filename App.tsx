a8a1c5a066343575fa284a771a91080d04a0a321
kailash gangai <kailash.gangai@cartmade.com>
2025-09-15 10:10:12 +0545

    socialmedia

--- App.tsx
+++ App.tsx
@@ -8,6 +8,7 @@ import { ShopifyCheckoutSheetProvider } from '@shopify/checkout-sheet-kit';
 import Toast from 'react-native-toast-message';
 import { PersistGate } from 'redux-persist/integration/react';
 import SkeletonLoading from './src/components/SkeletonLoading';
+import '@react-native-firebase/app';
 
 export default function App() {
   const isDarkMode = useColorScheme() === 'dark';
